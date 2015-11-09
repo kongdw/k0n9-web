723a9ee4e3b8c09f87f362f3e6d4763ec93d384f
kongdw <kong.dw@hotmail.com>
2015-11-09 23:18:14 +0800

    修改基础实体

---
+++ src/main/java/k0n9/common/entity/LogicDeleteable.java
@@ -0,0 +1,21 @@
+package k0n9.common.entity;
+
+/**
+ * <p>实体实现该接口表示想要逻辑删除
+ * 为了简化开发 约定删除标识列名为deleted，如果想自定义删除的标识列名：
+ * @author David Kong
+ *         date : 2015/11/9
+ * @version 1.0
+ */
+public interface LogicDeleteable {
+
+    public Boolean getDeleted();
+
+    public void setDeleted(Boolean deleted);
+
+    /**
+     * 标识为已删除
+     */
+    public void markDeleted();
+
+}
