0e9b17fb267bd0c7a45aecfebdfa75662b9119af
kongdw <kong.dw@hotmail.com>
2015-11-15 23:27:42 +0800

    修改基础实体

--- src/main/java/k0n9/common/web/json/DateTimeJsonSerializer.java
+++ src/main/java/k0n9/common/web/json/DateTimeJsonSerializer.java
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import lab.s2jh.core.util.DateUtils;
+import k0n9.common.utils.DateUtils;
 
 import java.io.IOException;
 import java.util.Date;
