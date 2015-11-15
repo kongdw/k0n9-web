2448011798e61ba769b932ae1b2660f500c6fbef
kongdw <kong.dw@hotmail.com>
2015-11-15 23:27:00 +0800

    修改基础实体

---
+++ src/main/java/k0n9/common/web/json/DateJsonSerializer.java
@@ -0,0 +1,21 @@
+package k0n9.common.web.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import lab.s2jh.core.util.DateUtils;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class DateJsonSerializer extends JsonSerializer<Date> {
+
+    @Override
+    public void serialize(Date value, JsonGenerator jgen, SerializerProvider provider) throws IOException,
+            JsonProcessingException {
+        if (value != null) {
+            jgen.writeString(DateUtils.formatDate(value));
+        }
+    }
+}
---
+++ src/main/java/k0n9/common/web/json/DateTimeJsonSerializer.java
@@ -0,0 +1,25 @@
+package k0n9.common.web.json;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import lab.s2jh.core.util.DateUtils;
+
+import java.io.IOException;
+import java.util.Date;
+
+public class DateTimeJsonSerializer extends JsonSerializer<Date> {
+
+    @Override
+    public void serialize(Date value, JsonGenerator jgen, SerializerProvider provider) throws IOException,
+            JsonProcessingException {
+        if (value != null) {
+            jgen.writeString(DateUtils.formatTime(value));
+        }
+    }
+
+    public Class<Date> handledType() {
+        return Date.class;
+    }
+}
